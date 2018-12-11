50246566c8c1fddfa808fd62adaba2b738465fbf
Bartek <suzonowicz@gmail.com>
2018-12-11 21:09:11 +0100

    Simple explosion animation, great background added.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,24 @@
 package sample;
 
-import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.util.Duration;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+
+import java.io.File;
+import java.util.Random;
 
 public class Main extends Application {
 
@@ -27,10 +33,12 @@ public class Main extends Application {
         Canvas canvas = new Canvas(800, 512);
         root.getChildren().add(canvas);
 
-        final GraphicsContext gc = canvas.getGraphicsContext2D();
+        GraphicsContext gc = canvas.getGraphicsContext2D();
 
-       // Image img = new Image("C:\\Users\\user\\IdeaProjects\\Tank1990\\Robo.jpg");
-       // gc.drawImage(img,0,0);
+        File RoboImageFile = new File("Robo.jpg");
+        Image RoboImage = new Image(RoboImageFile.toURI().toString());
+        gc.drawImage(RoboImage,0,0,canvas.getWidth(),canvas.getHeight());
+
         gc.setFill(Color.RED);
         gc.fill();
         gc.setStroke(Color.YELLOW);
@@ -38,7 +46,22 @@ public class Main extends Application {
         Font theFont = Font.font("Comic Sans", FontWeight.BOLD, 48);
         gc.setFont(theFont);
 
+        // Animated explosion (named UFO :P)
+        File file;
+        AnimatedImage ufo = new AnimatedImage();
+        Image[] imageArray = new Image[5];
+        for (int i = 1; i < 6; i++) {
+            file = new File("Resources/Explosion1/" + i + ".png");
+            imageArray[i - 1] = new Image(file.toURI().toString());
+        }
+        ufo.frames = imageArray;
+        ufo.duration = 0.750;
+
+
         final long startNanoTime = System.nanoTime();
+
+        Random random = new Random(13);
+
 
         new AnimationTimer()
         {
@@ -49,15 +72,65 @@ public class Main extends Application {
                 double x = 232 + 128 * Math.cos(t);
                 double y = 232 + 128 * Math.sin(t);
 
+                double explosionX = 0;
+                double explosionY = 0;
+
+                if((currentNanoTime - startNanoTime)%6 == 0)
+                {
+                    explosionX = random.nextInt((int)canvas.getWidth());
+                    explosionY = random.nextInt((int)canvas.getHeight());
+                }
+
+
                 gc.clearRect(0,0,canvas.getWidth(), canvas.getHeight());
+                gc.setGlobalAlpha(0.5);
+                gc.drawImage(RoboImage,0,0,canvas.getWidth(),canvas.getHeight());
+                gc.setGlobalAlpha(1);
                 gc.fillText("Привет, товарищ!", x, y);
                 gc.strokeText("Привет, товарищ!", x, y);
+                gc.drawImage( ufo.getFrame(t), explosionX, explosionY );
             }
         }.start();
 
         //primaryStage.setScene(theScene);
         primaryStage.show();
         Canvas c =new Canvas(500,300);
+
+//        primaryStage.setTitle( "AnimatedImage Example" );
+//
+//        Group root = new Group();
+//        Scene theScene = new Scene( root );
+//        primaryStage.setScene( theScene );
+//
+//        Canvas canvas = new Canvas( 512, 512 );
+//        root.getChildren().add( canvas );
+//
+//        GraphicsContext gc = canvas.getGraphicsContext2D();
+//
+//
+//        AnimatedImage ufo = new AnimatedImage();
+//        Image[] imageArray = new Image[6];
+//        for (int i = 0; i < 6; i++)
+//            imageArray[i] = new Image( "ufo_" + i + ".png" );
+//        ufo.frames = imageArray;
+//        ufo.duration = 0.100;
+//
+//        final long startNanoTime = System.nanoTime();
+//
+//        new AnimationTimer()
+//        {
+//            public void handle(long currentNanoTime)
+//            {
+//                double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+//
+//                double x = 232 + 128 * Math.cos(t);
+//                double y = 232 + 128 * Math.sin(t);
+//
+//                gc.drawImage( ufo.getFrame(t), 450, 25 );
+//            }
+//        }.start();
+//
+//        primaryStage.show();
     }
 
 
@@ -65,3 +138,4 @@ public class Main extends Application {
         launch(args);
     }
 }
+
