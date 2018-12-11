89a9ecba317d88ae326e60242ba457273ddc29d5
Bartek <suzonowicz@gmail.com>
2018-12-11 20:06:52 +0100

    Added "Hello friend" game over screen.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,63 @@
 package sample;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Group root = new Group();
         primaryStage.setTitle("Tank 1990");
-        primaryStage.setScene(new Scene(root, 800, 600));
+
+        Scene theScene = new Scene(root);
+        primaryStage.setScene(theScene);
+
+        Canvas canvas = new Canvas(800, 512);
+        root.getChildren().add(canvas);
+
+        final GraphicsContext gc = canvas.getGraphicsContext2D();
+
+       // Image img = new Image("C:\\Users\\user\\IdeaProjects\\Tank1990\\Robo.jpg");
+       // gc.drawImage(img,0,0);
+        gc.setFill(Color.RED);
+        gc.fill();
+        gc.setStroke(Color.YELLOW);
+        gc.setLineWidth(2);
+        Font theFont = Font.font("Comic Sans", FontWeight.BOLD, 48);
+        gc.setFont(theFont);
+
+        final long startNanoTime = System.nanoTime();
+
+        new AnimationTimer()
+        {
+            public void handle(long currentNanoTime)
+            {
+                double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+
+                double x = 232 + 128 * Math.cos(t);
+                double y = 232 + 128 * Math.sin(t);
+
+                gc.clearRect(0,0,canvas.getWidth(), canvas.getHeight());
+                gc.fillText("Привет, товарищ!", x, y);
+                gc.strokeText("Привет, товарищ!", x, y);
+            }
+        }.start();
+
+        //primaryStage.setScene(theScene);
         primaryStage.show();
         Canvas c =new Canvas(500,300);
-        Image img = new Image(300,200);
     }
 
 
